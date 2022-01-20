018c80b112c5b295ef85dc30dcb397c091bf5057
Brad Germain <bradg0620@icloud.com>
2022-01-20 10:01:22 -0500

    Added basic exception handling project

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's test exception handling and how nesting works with it!");
+            ExceptionMethod1();
+        }
+
+        public static void ExceptionMethod1()
+        {
+            try
+            {
+                ExceptionMethod2();
+                // throw new Exception("ExceptionMethod1 Message");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("ExceptionMethod1: " + ex.Message);
+            }
+        }
+        
+        public static void ExceptionMethod2()
+        {
+            try
+            {
+                ExceptionMethod3();
+                throw new Exception("ExceptionMethod2 Message");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("ExceptionMethod2: " + ex.Message);
+            }
+        }
+
+        public static void ExceptionMethod3()
+        {
+            try
+            {
+                throw new Exception("ExceptionMethod3 Message");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("ExceptionMethod3: " + ex.Message);
+                throw new Exception("ExceptionMethod3: Exception within catch");
+            }
+        }
+    }
+}
